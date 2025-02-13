3c85b5c7734cb1938ac90b4d702392cf5c4f5f2f
Geeck03 <aramaea.marie@gmail.com>
2025-02-13 15:47:56 -0600

    Initial Commit

---
+++ NotesApp/NotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppApp.swift
+//  NotesApp
+//
+//  Created by Sewell, Aramaea on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
