f10373b00fcfdf3bc1c8d506599b1d0c2605f464
Geeck03 <aramaea.marie@gmail.com>
2025-02-15 16:41:43 -0600

    Finished tutorial

---
+++ NotesApp/Views/AddNotesView.swift
@@ -0,0 +1,18 @@
+//
+//  AddNotesView.swift
+//  NotesApp
+//
+//  Created by Sewell, Aramaea on 2/15/25.
+//
+
+import SwiftUI
+
+struct AddNotesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddNotesView()
+}
